ff6d89ea2c114de99824f563491e85c7d14ad3c0
Sehi L'Yi <sehilyi@gmail.com>
2021-09-21 13:48:25 -0400

    Add sliders and buttons for reactive rendering

--- src/App.js
+++ src/App.js
@@ -1,181 +1,120 @@
 import './App.css';
-import React from 'react';
+import React, { useState } from 'react';
 import * as gosling from 'gosling.js';
 
-const goslingSpec = {
-  "arrangement": "vertical",
-  "views": [
-    {
-      "layout": "circular",
-      "tracks": [
-        {
-          "data": {
-            "url": "https://resgen.io/api/v1/tileset_info/?d=UvVPeLHuRDiYA3qwFlm7xQ",
-            "type": "multivec",
-            "row": "sample",
-            "column": "position",
-            "value": "peak",
-            "categories": ["sample 1", "sample 2", "sample 3", "sample 4"],
-            "binSize": 4
-          },
-          "x": {"field": "start", "type": "genomic"},
-          "xe": {"field": "end", "type": "genomic"},
-          "y": {"field": "peak", "type": "quantitative"},
-          "row": {"field": "sample", "type": "nominal"},
-          "color": {"field": "sample", "type": "nominal"},
-          "stroke": {"value": "black"},
-          "strokeWidth": {"value": 0.3},
-          "alignment": "overlay",
-          "tracks": [
-            {"mark": "bar"},
-            {
-              "mark": "brush",
-              "x": {"linkingId": "detail-1"},
-              "color": {"value": "blue"}
+const goslingSpec = (domain, mark, binSize, height) => { 
+  return {
+    "layout": "linear",
+    "arrangement": "vertical",
+    "centerRadius": 0.8,
+    "xDomain": {"chromosome": "1", "interval": [1, 3000500]},
+    "views": [
+      {
+        "tracks": [
+          {
+            "id": "track-1",
+            "data": {
+              "url": "https://server.gosling-lang.org/api/v1/tileset_info/?d=cistrome-multivec",
+              "type": "multivec",
+              "row": "sample",
+              "column": "position",
+              "value": "peak",
+              "categories": ["sample 1", "sample 2", "sample 3", "sample 4"],
+              binSize: binSize === 0 ? 1 : binSize
             },
-            {
-              "mark": "brush",
-              "x": {"linkingId": "detail-2"},
-              "color": {"value": "red"}
-            }
-          ],
-          "style": {"outlineWidth": 0},
-          "width": 500,
-          "height": 100
-        },
-        {
-          "data": {
-            "type": "csv",
-            "url": "https://raw.githubusercontent.com/sehilyi/gemini-datasets/master/data/rearrangements.bulk.1639.simple.filtered.pub",
-            "headerNames": [
-              "chr1",
-              "p1s",
-              "p1e",
-              "chr2",
-              "p2s",
-              "p2e",
-              "type",
-              "id",
-              "f1",
-              "f2",
-              "f3",
-              "f4",
-              "f5",
-              "f6"
+            "mark": mark,
+            "x": {"field": "start", "type": "genomic", "axis": "top"},
+            "xe": {"field": "end", "type": "genomic"},
+            "row": {"field": "sample", "type": "nominal", "legend": true},
+            "color": {"field": "peak", "type": "quantitative", "legend": true, domain},
+            "tooltip": [
+              {"field": "start", "type": "genomic", "alt": "Start Position"},
+              {"field": "end", "type": "genomic", "alt": "End Position"},
+              {
+                "field": "peak",
+                "type": "quantitative",
+                "alt": "Value",
+                "format": ".2"
+              },
+              {"field": "sample", "type": "nominal", "alt": "Sample"}
             ],
-            "separator": "\t",
-            "genomicFieldsToConvert": [
-              {"chromosomeField": "chr1", "genomicFields": ["p1s", "p1e"]},
-              {"chromosomeField": "chr2", "genomicFields": ["p2s", "p2e"]}
-            ]
-          },
-          "dataTransform": [
-            {"type": "filter", "field": "chr1", "oneOf": ["1", "16", "14", "9", "6", "5", "3"]},
-            {"type": "filter", "field": "chr2", "oneOf": ["1", "16", "14", "9", "6", "5", "3"]}
-          ],
-          "mark": "link",
-          "x": {"field": "p1s", "type": "genomic"},
-          "xe": {"field": "p1e", "type": "genomic"},
-          "x1": {"field": "p2s", "type": "genomic"},
-          "x1e": {"field": "p2e", "type": "genomic"},
-          "stroke": {
-            "field": "type",
-            "type": "nominal",
-            "domain": [
-              "deletion",
-              "inversion",
-              "translocation",
-              "tandem-duplication"
-            ]
-          },
-          "strokeWidth": {"value": 0.8},
-          "opacity": {"value": 0.15},
-          "style": {"circularLink": true},
-          "width": 500,
-          "height": 100
-        }
-      ]
-    },
-    {
-      "spacing": 10,
-      "arrangement": "horizontal",
-      "views": [
-        {
-          "tracks": [
-            {
-              "data": {
-                "url": "https://resgen.io/api/v1/tileset_info/?d=UvVPeLHuRDiYA3qwFlm7xQ",
-                "type": "multivec",
-                "row": "sample",
-                "column": "position",
-                "value": "peak",
-                "categories": ["sample 1", "sample 2", "sample 3", "sample 4"],
-                "binSize": 4
-              },
-              "mark": "bar",
-              "x": {
-                "field": "start",
-                "type": "genomic",
-                "linkingId": "detail-1",
-                "domain": {"chromosome": "5"}
-              },
-              "xe": {"field": "end", "type": "genomic"},
-              "y": {"field": "peak", "type": "quantitative"},
-              "row": {"field": "sample", "type": "nominal"},
-              "color": {"field": "sample", "type": "nominal"},
-              "stroke": {"value": "black"},
-              "strokeWidth": {"value": 0.3},
-              "style": {"background": "blue", "backgroundOpacity": 0.1},
-              "width": 245,
-              "height": 150
-            }
-          ]
-        },
-        {
-          "tracks": [
-            {
-              "data": {
-                "url": "https://resgen.io/api/v1/tileset_info/?d=UvVPeLHuRDiYA3qwFlm7xQ",
-                "type": "multivec",
-                "row": "sample",
-                "column": "position",
-                "value": "peak",
-                "categories": ["sample 1", "sample 2", "sample 3", "sample 4"],
-                "binSize": 4
-              },
-              "mark": "bar",
-              "x": {
-                "field": "start",
-                "type": "genomic",
-                "domain": {"chromosome": "16"},
-                "linkingId": "detail-2"
-              },
-              "xe": {"field": "end", "type": "genomic"},
-              "y": {"field": "peak", "type": "quantitative"},
-              "row": {"field": "sample", "type": "nominal"},
-              "color": {"field": "sample", "type": "nominal", "legend": true},
-              "stroke": {"value": "black"},
-              "strokeWidth": {"value": 0.3},
-              "style": {"background": "red", "backgroundOpacity": 0.1},
-              "width": 245,
-              "height": 150
-            }
-          ]
-        }
-      ]
-    }
-  ]
+            "width": 600,
+            height
+          }
+        ]
+      },
+    ]
+  }
 };
+
 function App() {
+
+  const [min, setMin] = useState(0);
+  const [height, setHeight] = useState(130);
+  const [mark, setMark] = useState('rect');
+  const [binSize, setBinSize] = useState(0);
+
   return (
-    
+    <>
+      <span>
+        <div style={{marginTop: 30, marginLeft: 80}}>
+          Bin Size:
+          <input 
+            type="range" 
+            min={0}
+            max={32}
+            step={4} 
+            value={binSize}
+            className="slider" 
+            id="bin-slider" 
+            style={{ width: 100, display: 'inline', margin: 10}}
+            onChange={(e) => setBinSize(+e.currentTarget.value) }
+          />
+          {binSize === 0 ? 1 : binSize}
+        </div>
+        <div style={{marginLeft: 80}}>
+          Color Min Value:
+          <input 
+            type="range" 
+            min={0}
+            max={0.001}
+            step={0.0001} 
+            value={min}
+            className="slider" 
+            id="min-slider" 
+            style={{ width: 100, display: 'inline', margin: 10}}
+            onChange={(e) => setMin(e.currentTarget.value) }
+          />
+          {min}
+        </div>
+        {/* <div style={{marginLeft: 80}}>
+          Height:
+          <input 
+            type="range" 
+            min={130}
+            max={300}
+            step={10} 
+            value={height}
+            className="slider" 
+            id="height-slider" 
+            style={{ width: 100, display: 'inline', margin: 10}}
+            onChange={(e) => setHeight(e.currentTarget.value) }
+          />
+          {height}
+        </div> */}
+      </span>
+      <div style={{marginTop: 30, marginLeft: 80}}>
+        Mark:
+        <select name="mark" onChange={(e) => setMark(e.currentTarget.value)}>
+          <option value="rect">rect</option>
+          <option value="point">point</option>
+        </select>
+      </div>
       <gosling.GoslingComponent
-        compiled={(a, b) => {
-          // just compiled
-        }}
-        spec={goslingSpec}
+        spec={goslingSpec([+min, 0.001], mark, binSize, height)}
+        experimental={{ reactive: true }}
       />
-    // </div>
+    </>
   );
 }
 
