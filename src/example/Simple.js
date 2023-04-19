a9a21ab528b442b4ebf5778bb643993deb3d2c1d
ThHarbig <theresa_ha@hotmail.de>
2023-04-19 14:02:52 -0400

    Revert SimpleExample to original version

--- src/example/Simple.js
+++ src/example/Simple.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { GoslingComponent } from 'gosling.js';
+import {GoslingComponent} from 'gosling.js';
 
 function SimpleExample() {
 	return (
@@ -9,12 +9,19 @@ function SimpleExample() {
 					tracks: [{
 						id: 'heatmap-track',
 						data: {
-							url: 'https://raw.githubusercontent.com/ThHarbig/gosling-react/master/proteins_167_161521.csv',
-							type: 'csv',
-							chromosomeField: 'Locus tag',
-							genomicFields: ['Start', 'Stop']
+							url: 'https://server.gosling-lang.org/api/v1/tileset_info/?d=cistrome-multivec',
+							type: 'multivec',
+							row: 'sample',
+							column: 'position',
+							value: 'peak',
+							categories: ['sample 1', 'sample 2', 'sample 3', 'sample 4'],
+							binSize: 4
 						},
 						mark: 'rect',
+						x: { field: 'start', type: 'genomic' },
+						xe: { field: 'end', type: 'genomic' },
+						row: { field: 'sample', type: 'nominal', legend: true },
+						color: { field: 'peak', type: 'quantitative', legend: true, range: 'pink' },
 						width: 600,
 						height: 130
 					}]
