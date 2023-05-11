bb5e31866c18dce232d31839c624ea699e46b262
sehilyi <sehilyi@gmail.com>
2023-05-11 14:26:19 -0400

    chore: more updates

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    build: { target: 'esnext' },
+    build: { target: 'esnext', outDir: 'build' },
     optimizeDeps: {
         include: ['gosling.js']
     },
