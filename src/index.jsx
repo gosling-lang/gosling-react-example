4e2b93843fe0bac35e2db9021cae1a8cfdbb3d8c
sehilyi <sehilyi@gmail.com>
2023-05-11 18:17:15 -0400

    feat: use react 18

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
 	<HashRouter>
 		<App/>
-	</HashRouter>,
-	document.getElementById('root')
+	</HashRouter>
 );
