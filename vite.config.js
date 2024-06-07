e11178bc255af6bb4653f9923052e326bf55b720
etowahadams <etowahadams@gmail.com>
2024-06-07 16:29:03 -0400

    feat: base url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import reactRefresh from '@vitejs/plugin-react-refresh';
 // https://vitejs.dev/config/
 export default defineConfig({
     build: { target: 'esnext', outDir: 'build' },
-    plugins: [reactRefresh()]
+    plugins: [reactRefresh()],
+    base: "/gosling-react-example/"
 });
